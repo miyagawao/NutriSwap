ea1ed4a41ba64c327bcb57a7e52988fe72134af6
miyagawao <ec2-user@ip-172-31-31-224.ap-northeast-1.compute.internal>
2023-04-19 10:56:11 +0000

    contributorフォルダ削除

--- app/controllers/contributor/sessions_controller.rb
+++
@@ -1,7 +0,0 @@
-class Contributors::SessionsController < Devise::SessionsController
-  def guest_sign_in
-    contributor = Contributor.guest
-    sign_in contributor
-    redirect_to contributor_path(contributor), notice: 'ゲストユーザーとしてログインしました。'
-  end
-end
