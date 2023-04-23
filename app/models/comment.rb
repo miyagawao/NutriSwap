7b3191db0e7221662c8f68927240f1808c6601da
miyagawao <ec2-user@ip-172-31-31-224.ap-northeast-1.compute.internal>
2023-04-23 08:55:15 +0000

    Fix コメント削除

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :contributor
   belongs_to :post
   belongs_to :parent, class_name: "Comment", optional: true
-  has_many :replies, class_name: "Comment", foreign_key: :parent_id
+  has_many :replies, class_name: "Comment", foreign_key: :parent_id, dependent: :destroy
   has_many :reports
 
   validates :comment_text, presence: true
